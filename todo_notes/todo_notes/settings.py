a24a68e2a70c89d64e860609e6d693fe62f4bf01
Vladimir_Novikov <vovasnew@mail.ru>
2021-12-18 12:24:48 +0500

    размер страницы для всех api 100 записей

--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -83,7 +83,9 @@ WSGI_APPLICATION = "todo_notes.wsgi.application"
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
 }
 
 if DEBUG:
