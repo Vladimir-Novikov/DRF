ed978c31ad4d73a015bc62f32e6cbb7aadcec38f
Vladimir_Novikov <vovasnew@mail.ru>
2021-12-18 15:33:54 +0500

    классы пагинаций для project и todo

--- todo_notes/todoapp/views.py
+++ todo_notes/todoapp/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 from .models import Project, Todo
 from .serializers import TodoModelSerializer, ProjectModelSerializer
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    """класс пагинации для Project"""
+
+    default_limit = 10
+
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    """класс пагинации для Todo"""
+
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    pagination_class = TodoLimitOffsetPagination
