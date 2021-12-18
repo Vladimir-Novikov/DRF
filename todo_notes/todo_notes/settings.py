10dd57ab7d3a5ddcfffee9080f7cff60ac888add
Vladimir_Novikov <vovasnew@mail.ru>
2021-12-18 21:13:48 +0500

    filters

--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "userapp",
     "todoapp",
     "corsheaders",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 if DEBUG:
--- todo_notes/todoapp/views.py
+++ todo_notes/todoapp/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
 from .models import Project, Todo
 from .serializers import TodoModelSerializer, ProjectModelSerializer
+from .filters import ProjectFilter
 
 
 class ProjectLimitOffsetPagination(LimitOffsetPagination):
@@ -22,6 +23,7 @@ class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
     pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TodoModelViewSet(ModelViewSet):
@@ -30,6 +32,7 @@ class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
     pagination_class = TodoLimitOffsetPagination
+    filterset_fields = ["project"]
 
     def perform_destroy(self, instance):
         instance.is_active = False
