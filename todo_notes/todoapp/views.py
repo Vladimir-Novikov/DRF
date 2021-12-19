085c02d468e278bcda4d7bab15b4de1d542cbfd8
Vladimir_Novikov <vovasnew@mail.ru>
2021-12-19 16:59:03 +0500

    фильтрация для модели todo

--- todo_notes/todoapp/views.py
+++ todo_notes/todoapp/views.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
 from .models import Project, Todo
 from .serializers import TodoModelSerializer, ProjectModelSerializer
-from .filters import ProjectFilter
+from .filters import ProjectFilter, TodoFilter
 
 
 class ProjectLimitOffsetPagination(LimitOffsetPagination):
@@ -32,7 +32,7 @@ class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
     pagination_class = TodoLimitOffsetPagination
-    filterset_fields = ["project"]
+    filterset_class = TodoFilter
 
     def perform_destroy(self, instance):
         instance.is_active = False
