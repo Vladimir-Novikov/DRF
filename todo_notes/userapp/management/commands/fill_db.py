11db64050133f0df9a9f1dcb5902d92c50cd4811
Vladimir_Novikov <vovasnew@mail.ru>
2021-12-26 15:16:21 +0500

    доработка fill_db таблица с правами пишется из скрипта. бд не нужно отпраdлять в пр

--- todo_notes/userapp/management/commands/fill_db.py
+++ todo_notes/userapp/management/commands/fill_db.py
@@ -3,6 +3,7 @@ import os
 
 from sys import platform
 
+import sqlite3
 
 from django.conf import settings
 from django.core.management import BaseCommand
@@ -66,6 +67,59 @@ def load_todos():
         Todo.objects.create(project=project, text=todo["text"], user=user, is_active=todo["is_active"])
 
 
+def fill_permissions_tables():
+
+    """заполнение таблиц с группами и правами групп пользователей"""
+
+    print("Заполнение таблиц с группами пользователей...")
+
+    groups = [("1", "Администраторы"), ("2", "Разработчики"), ("3", "Владельцы проектов")]
+
+    group_permissions = [
+        (1, 1, 32),
+        (2, 1, 21),
+        (3, 1, 22),
+        (4, 1, 23),
+        (5, 1, 24),
+        (7, 1, 26),
+        (8, 1, 27),
+        (9, 1, 28),
+        (10, 1, 29),
+        (11, 1, 30),
+        (12, 1, 31),
+        (13, 2, 32),
+        (14, 2, 24),
+        (15, 2, 28),
+        (16, 2, 29),
+        (17, 2, 30),
+        (18, 2, 31),
+        (19, 3, 32),
+        (20, 3, 24),
+        (21, 3, 25),
+        (22, 3, 26),
+        (23, 3, 27),
+        (24, 3, 28),
+        (25, 3, 29),
+        (26, 3, 30),
+        (27, 3, 31),
+        (28, 1, 25),
+    ]
+
+    user_groups = [(1, 7, 3), (2, 3, 3), (3, 6, 3), (4, 10, 2), (5, 4, 2), (6, 5, 2), (7, 2, 1)]
+
+    conn = sqlite3.connect("db.sqlite3")
+    cursor = conn.cursor()
+
+    cursor.executemany("INSERT INTO auth_group VALUES(?, ?);", groups)
+    conn.commit()
+
+    cursor.executemany("INSERT INTO auth_group_permissions VALUES(?, ?, ?);", group_permissions)
+    conn.commit()
+
+    cursor.executemany("INSERT INTO userapp_user_groups VALUES(?, ?, ?);", user_groups)
+    conn.commit()
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
@@ -73,5 +127,6 @@ class Command(BaseCommand):
         load_users()
         load_projects()
         load_todos()
+        fill_permissions_tables()
 
         print("Загрузка данных завершена.")
